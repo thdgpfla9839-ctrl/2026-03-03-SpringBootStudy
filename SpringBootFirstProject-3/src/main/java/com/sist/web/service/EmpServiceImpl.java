765cf3fe5c930c5b5aa29cf0e76c1a098f5c6e0e
thdgpfla9839-ctrl <thdgpfla9839@gmail.com>
2026-08-12 15:59:06 +0900

    스프링부트에서 DB Emp테이블을 이용해 이름과 직업을 목록 출력하는 페이지 실습

---
+++ SpringBootFirstProject-3/src/main/java/com/sist/web/service/EmpServiceImpl.java
@@ -0,0 +1,23 @@
+package com.sist.web.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sist.web.mapper.EmpMapper;
+import com.sist.web.vo.EmpVO;
+
+@Service
+public class EmpServiceImpl implements EmpService{
+
+	@Autowired
+	private EmpMapper mapper;
+
+	@Override
+	public List<EmpVO> empListData() {
+		// TODO Auto-generated method stub
+		return mapper.empListData();
+	}
+	
+}
